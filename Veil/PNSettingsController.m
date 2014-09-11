1e27e6b945b15c55a2ed9c1806d531711634f761
soojin.ro <sugarpoint33@gmail.com>
2014-09-12 01:15:16 +0900

    before tableview fix

--- Veil/PNSettingsController.m
+++ Veil/PNSettingsController.m
@@ -33,10 +33,12 @@
     [super viewDidLoad];
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"delete cookie" style:UIBarButtonItemStylePlain target:self action:@selector(deleteCookie)];
+    //self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"delete cookie" style:UIBarButtonItemStylePlain target:self action:@selector(deleteCookie)];
     
     //Remove unneccessary separators
     self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    self.tableView.separatorInset = UIEdgeInsetsMake(0, 0, 0, 0);
     
     _initialValue = [[NSUserDefaults standardUserDefaults] boolForKey:kShouldRegisterPushKey];
     if (_initialValue) {
